46745a17222b1ce9e2e6fe2fd7cecde7dbdd1262
lumi192 <robertsoncruz192@gmail.com>
2025-10-02 17:35:03 +0800

    Created a failsafe for exiting

--- prototype.py
+++ prototype.py
@@ -11,7 +11,7 @@ def RR(y_true, y_pred):
 exit = "N"
 
 while exit == "N":
-    available = [
+    available_function_types = [
         "Linear",
         "Quadratic",
         "Cubic",
@@ -23,105 +23,123 @@ while exit == "N":
         "Hyperbolic",
     ]
 
+    available_perceptron_counts = [
+        32,
+        64,
+        128
+    ]
+
     print(". . .prototype_alpha_v1.1 by drdt192. . .\n\n")
 
     while True:
         try:
-            perceptron_count = int(input(f"How many perceptrons are within the single hidden layer? Choose from 32, 64, and 128!\n"))
+            perceptron_count = int(input(f'>>>CHOOSE YOUR PERCEPTRON COUNT:\n{available_perceptron_counts}\n\n'))
         except ValueError:
-            print("That is not a number!")
+            print("That is not a number! Please try again!\n")
             continue
-        if perceptron_count in [32, 64, 128]: break
-        print("That isn't a valid perceptron count!")
+        if perceptron_count in available_perceptron_counts: break
+        print("That is not a valid perceptron count! Please try again!\n")
 
 
     while True:
-        function = input(f"Hello! Which scalar mathematical function would you like the FNN to learn?\n {available}\n")
-        if function in available: break
+        function = input(f">>>CHOOSE YOUR SCALAR MATHEMATICAL FUNCTION TYPE:\n{available_function_types}\n\n")
+        if function in available_function_types: break
         print("That is not a valid function! Please try again!\n")
 
     while True:
         try:
-            trials = int(input("How many trials would you like to do? Choose from 1 to 100!\n"))
+            trials = int(input(">>>CHOOSE YOUR TRIAL COUNT: 1 TO 100\n\n"))
         except ValueError:
-            print("That is not a number!\n") 
+            print("That is not a number! Please try again!\n") 
             continue
         if trials in list(range(1, 101)): break
-        print("That is not a valid number of trials! Please try again!\n")
+        print("That is not a valid trial count! Please try again!\n")
 
     trial = 1
     results = []
 
-    print("Starting... Please wait until 99.99% for each trial!")
+    print("Starting... Please wait until 99.99% for each trial! PRESS CTRL+C TO EXIT!")
     while trial <= trials:
-        class FNN(nn.Module):
-            def __init__(self) -> None:
-                super().__init__()
+        try:
+            class FNN(nn.Module):
+                def __init__(self) -> None:
+                    super().__init__()
 
-                self.net = nn.Sequential(
-                    nn.Linear(1, perceptron_count),
-                    nn.ReLU(), 
-                    nn.Linear(perceptron_count, 1), 
-                )
+                    self.net = nn.Sequential(
+                        nn.Linear(1, perceptron_count),
+                        nn.ReLU(), 
+                        nn.Linear(perceptron_count, 1), 
+                    )
     
-            def forward(self, input):
-                return self.net(input)
-        model = FNN()
+                def forward(self, input):
+                    return self.net(input)
+            model = FNN()
 
-        x_np = np.linspace(start=1, stop=10, num=1000)
-        y_np = {
-            "Linear": np.polyval(p=[1, 0], x=x_np),
-            "Quadratic": np.polyval(p=[1, 0, 0], x=x_np),
-            "Cubic": np.polyval(p=[1, 0, 0, 0], x=x_np),
-            "Rational":  1 / np.polyval(p=[1, 0], x=x_np),
-            "Exponential": np.exp(x_np),
-            "Radical": np.sqrt(x_np),
-            "Logarithmic": np.log(x_np),
-            "Trigonometric": np.sin(x_np),
-            "Hyperbolic": np.sinh(x_np),
-        }
+            x_np = np.linspace(start=1, stop=10, num=1000)
+            y_np = {
+                "Linear": np.polyval(p=[1, 0], x=x_np),
+                "Quadratic": np.polyval(p=[1, 0, 0], x=x_np),
+                "Cubic": np.polyval(p=[1, 0, 0, 0], x=x_np),
+                "Rational":  1 / np.polyval(p=[1, 0], x=x_np),
+                "Exponential": np.exp(x_np),
+                "Radical": np.sqrt(x_np),
+                "Logarithmic": np.log(x_np),
+                "Trigonometric": np.sin(x_np),
+                "Hyperbolic": np.sinh(x_np),
+            }
 
-        x = torch.tensor(data=x_np, dtype=torch.float32)
-        y = torch.tensor(data=y_np[function], dtype=torch.float32)
-        x_dataset = x.unsqueeze(1)
-        y_dataset = y.unsqueeze(1)
+            x = torch.tensor(data=x_np, dtype=torch.float32)
+            y = torch.tensor(data=y_np[function], dtype=torch.float32)
+            x_dataset = x.unsqueeze(1)
+            y_dataset = y.unsqueeze(1)
 
-        loss_function = nn.MSELoss()
-        optimizer = optim.Adam(params=model.parameters(), lr=0.1)
+            loss_function = nn.MSELoss()
+            optimizer = optim.Adam(params=model.parameters(), lr=0.1)
 
-        epochs = 0
-        while epochs < 1_000_000:
-            epochs += 1
-            model.train()
-            optimizer.zero_grad()
-            FNN_output = model(x_dataset)
+            epochs = 0
+            while epochs < 1_000_000:
+                epochs += 1
+                model.train()
+                optimizer.zero_grad()
+                FNN_output = model(x_dataset)
 
-            r2 = RR(y_dataset, FNN_output.detach())
-            if r2 >= 0.9999:
+                r2 = RR(y_dataset, FNN_output.detach())
+                if r2 >= 0.9999:
+                    break
+    
+                loss = loss_function(FNN_output, y_dataset)
+                loss.backward()
+                optimizer.step()
+
+                if (epochs + 1) % 10 == 0:
+                    print(f"EPOCH:{epochs}, LOSS:{loss.item():.6f}, R^2:{r2 * 100:.6f}% - CTRL+C TO EXIT!")
+
+            print(f">FUNCTION TYPE: {function}")
+            print(f">FINAL NUMBER OF EPOCHS: {epochs}, TRIAL#{trial}\n\n")
+            results.append(epochs)
+            trial += 1
+        except KeyboardInterrupt:
+            while True:
+                prompt = input(">>>Would you to EXIT the experiment? Please type 'Y' or 'N'.\n")
+                if prompt in ["Y", "N"]: 
+                    exit = prompt
+                    break
+                print("That is not a valid input!\n")
+            if prompt == "Y":
+                print(f"INTERRUPTED AT TRIAL#{trial}\n")
                 break
-    
-            loss = loss_function(FNN_output, y_dataset)
-            loss.backward()
-            optimizer.step()
-
-            if (epochs + 1) % 10 == 0:
-                print(f"EPOCH:{epochs}, LOSS:{loss.item():.6f}, R^2:{r2 * 100:.6f}%")
-
-        print(f">FUNCTION TYPE: {function}")
-        print(f">FINAL NUMBER OF EPOCHS: {epochs}, TRIAL#{trial}\n\n")
-        results.append(epochs)
-        trial += 1
+            
 
     mean = np.mean(results)
     residuals = [(result - mean)**2 for result in results]
     variance = np.mean(residuals)
     std_var = np.sqrt(variance)
     print(f"Here are the results! Please copy.\n{results}\n")
-    print(f"FUNCTION: {function}, PERCEPTRONS: {perceptron_count}, TRIALS: {trials}\n")
+    print(f"FUNCTION: {function}, PERCEPTRONS: {perceptron_count}, TRIALS COMPLETED: {len(results)}/{trials}")
     print(f"MEAN: {mean}, VAR: {variance}, STD_VAR: {std_var}\n")
 
     while True:
-        prompt = input("Would you like to use the prototype again? Please type 'Y' or 'N'.\n")
+        prompt = input(">>>Would you like to exit? Please type 'Y' or 'N'.\n")
         if prompt in ["Y", "N"]: 
             exit = prompt
             break
